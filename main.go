defbb8445fabedb847a6a9d079c173d990b06e7a
Marcus Doescher <deh.marcus@outlook.de>
2022-10-14 23:20:01 +0200

    project: make request

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := &http.Client{}
+
+	username := "BarackObama"
+	req, err := http.NewRequest("GET", "https://api.twitter.com/2/users/by/username/"+username, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Add("Authorization", "Bearer "+os.Getenv("BEARER"))
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	fmt.Println(resp.StatusCode)
+	fmt.Println(resp.Body)
 }
