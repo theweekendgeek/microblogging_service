281573aca6ff5f72f01a18027c55a0007f69643d
Marcus Doescher <deh.marcus@outlook.de>
2022-10-17 20:01:39 +0200

    feat(request): write test for parsing Timeline

--- twitter_test/parser_test.go
+++ twitter_test/parser_test.go
@@ -7,9 +7,13 @@ import (
 )
 import "doescher.ninja/twitter-service/twitter"
 
+var parser twitter.Parser
+
+func init() {
+	parser = twitter.Parser{}
+}
+
 func TestParseProfileResponse(t *testing.T) {
-	type Timeline struct{}
-	parser := twitter.Parser{}
 	var profileResponse twitter.ProfileResponse
 	response := []byte(`
 		{
@@ -40,6 +44,98 @@ func TestParseProfileResponse(t *testing.T) {
 
 }
 
-func TestParsesProfiles(t *testing.T) {
+func TestParsesTimeLines(t *testing.T) {
+	response := []byte(`{
+	    "data": [
+	        {
+	            "edit_history_tweet_ids": [
+	                "1582051114941566976"
+	            ],
+	            "id": "1582051114941566976",
+	            "text": "Your delivery is being prepared. 📦\n\nNorthrop Grumman's next cargo mission to the @Space_Station will deliver new experiments studying the dynamics of mudflows, growing crops in space, and fertility treatments—all for the benefit of humanity: https://t.co/bGCuPQv6a1 https://t.co/JLZWuwCJJx"
+	        },
+	        {
+	            "edit_history_tweet_ids": [
+	                "1581628790878281729"
+	            ],
+	            "id": "1581628790878281729",
+	            "text": "RT @NASASolarSystem: ✅NASA’s #LucyMission completed its 1st Earth Gravity Assist, passing just 220 miles/350 km above Earth’s surface!\n\nIn…"
+	        },
+	        {
+	            "edit_history_tweet_ids": [
+	                "1581389969154658304"
+	            ],
+	            "id": "1581389969154658304",
+	            "text": "Our workforce is our strength. Check out our Twitter Moment for a recap of #HispanicHeritageMonth and #MesDeLaHerenciaHispana 2022, and follow @NASA_es for Spanish-language coverage all year round. https://t.co/YsLuM7u5vJ"
+	        },
+	        {
+	            "edit_history_tweet_ids": [
+	                "1581320295318622209"
+	            ],
+	            "id": "1581320295318622209",
+	            "text": "“I try to help people see that there’s an opportunity, and that if you work hard and are willing to evoke whatever it takes to get there, you can get it.”\nAndres Rivera is a systems engineer for the @EuropaClipper mission. Celebrate #HispanicHeritageMonth: https://t.co/6jZCsmwUD5 https://t.co/NDklOV5wLh"
+	        },
+	        {
+	            "edit_history_tweet_ids": [
+	                "1581302372453605377"
+	            ],
+	            "id": "1581302372453605377",
+	            "text": "\"The last time I was home, I found a little note that I had written when I was a 13-year-old kid. It said, 'My dream job in the future is to be a programmer.'\"\nDr. Ivan Perez Dominguez is a research scientist at @NASAAmes. Celebrate #HispanicHeritageMonth: https://t.co/d58POvHyd8 https://t.co/EVVt21xYvg"
+	        },
+	        {
+	            "edit_history_tweet_ids": [
+	                "1581068720092631040"
+	            ],
+	            "id": "1581068720092631040",
+	            "text": "This Week at NASA:\n\n• Confirmed! DART’s impact changed the orbit of asteroid Dimorphos\n• #Crew4 heads to Earth after 170 days in space\n• @NASAWebb sees cosmic \"tree rings\"\n• #Artemis I sets a new launch date\n\nWant more space in your life? Subscribe: https://t.co/MyG37QAe7m https://t.co/tQsM528BP3"
+	        },
+	        {
+	            "edit_history_tweet_ids": [
+	                "1581056289282482176"
+	            ],
+	            "id": "1581056289282482176",
+	            "text": "RT @nasahqphoto: Welcome home! Crew-4 NASA astronauts @astro_watkins, @astro_farmerbob, @astro_kjell, and @ESA’s @AstroSamantha are seen in…"
+	        },
+	        {
+	            "edit_history_tweet_ids": [
+	                "1581050294208581633"
+	            ],
+	            "id": "1581050294208581633",
+	            "text": "LIVE: Tune in for an update from NASA and @SpaceX leaders reviewing today's #Crew4 splashdown off the coast of Florida: https://t.co/IRdNh3Bqb7"
+	        },
+	        {
+	            "edit_history_tweet_ids": [
+	                "1581046644467847168"
+	            ],
+	            "id": "1581046644467847168",
+	            "text": "75 years ago, NACA—NASA’s predecessor—broke the sound barrier in our first experimental aircraft, the X-1. Now, via @NASAAero’s Quesst mission, we aim to do it again, but much quieter, to bring a return to commercial supersonic flight over land: https://t.co/LOWWTbFpSu https://t.co/Ezoi6Qboib"
+	        },
+	        {
+	            "edit_history_tweet_ids": [
+	                "1581043668206891008"
+	            ],
+	            "id": "1581043668206891008",
+	            "text": "After 170 days in orbit, our SpaceX Crew-4 astronauts safely splashed down at 4:55pm ET (20:55 UTC) Friday off the coast of Jacksonville, Florida, completing our fourth @Commercial_Crew mission to the International @Space_Station: https://t.co/XLOeKCjrTM https://t.co/yH5KYONXZa"
+	        }
+	    ],
+	    "meta": {
+	        "next_token": "7140dibdnow9c7btw423wugb5ysor0cpuq8gmvxycpcmw",
+	        "result_count": 10,
+	        "newest_id": "1582051114941566976",
+	        "oldest_id": "1581043668206891008"
+	    }
+	 }`)
+	var TimeLineResponse = twitter.TimelineResponse{}
+
+	_ = parser.ParseTimeline(response, &TimeLineResponse)
+
+	if assert.NotNil(t, TimeLineResponse.Data) && assert.NotNil(t, TimeLineResponse.MetaData) {
+		assert.Equal(t, TimeLineResponse.Data[0].Id, "1582051114941566976")
+		assert.Equal(t, TimeLineResponse.Data[0].Text, "Your delivery is being prepared. 📦\n\nNorthrop Grumman's next cargo mission to the @Space_Station will deliver new experiments studying the dynamics of mudflows, growing crops in space, and fertility treatments—all for the benefit of humanity: https://t.co/bGCuPQv6a1 https://t.co/JLZWuwCJJx")
+		assert.Equal(t, TimeLineResponse.Data[6].Id, "1581056289282482176")
+		assert.Equal(t, TimeLineResponse.Data[6].Text, "RT @nasahqphoto: Welcome home! Crew-4 NASA astronauts @astro_watkins, @astro_farmerbob, @astro_kjell, and @ESA’s @AstroSamantha are seen in…")
+		assert.Equal(t, TimeLineResponse.MetaData.NextToken, "7140dibdnow9c7btw423wugb5ysor0cpuq8gmvxycpcmw")
+		assert.Equal(t, TimeLineResponse.MetaData.ResultCount, 10)
+	}
 
 }
