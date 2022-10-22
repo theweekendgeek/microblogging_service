72a3b8f0b22858276cc0e9235b4545814c3bbd5c
Marcus Doescher <deh.marcus@outlook.de>
2022-10-22 09:41:34 +0200

    devops: move some tf values to var file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,33 @@
+variable "uri" {
+	type = string
+}
+
+variable "credentials" {
+	type = string
+}
+
 provider "google" {
   project     = "twitter-service-365615"
   region      = "europe-west1"
   zone        = "europe-west1-b"
-  credentials = "./twitter-service-365615-c5315f98529c.json"
+  credentials = var.credentials
 }
 
 resource "google_cloud_scheduler_job" "job" {
-	name             = "test-job"
-	description      = "test http job"
-	schedule         = "*/30 */4 * * *"
-	time_zone        = "America/New_York"
-	attempt_deadline = "320s"
+  name             = "test-job"
+  description      = "test http job"
+  schedule         = "0 */1 * * *"
+  time_zone        = "Europe/Berlin"
+  attempt_deadline = "320s"
 
-	retry_config {
-		retry_count = 1
-	}
+  retry_config {
+    retry_count = 1
+  }
 
-	http_target {
-		http_method = "GET"
-		uri         = "https://us-central1-twitter-service-365615.cloudfunctions.net/twitter-service"
-	}
+
+
+  http_target {
+    http_method = "GET"
+    uri         = var.uri
+  }
 }
